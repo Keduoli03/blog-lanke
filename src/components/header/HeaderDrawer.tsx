02865fc2b5c532947950c4df56e1b8d0521415ec
Lanke <2801429414@qq.com>
2026-02-25 21:49:02 +0800

    fix: 构建失败

--- src/components/header/HeaderDrawer.tsx
+++ src/components/header/HeaderDrawer.tsx
@@ -2,7 +2,6 @@ import { menus } from '@/config.json'
 import { createContext, useContext, useState, forwardRef } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { motion, AnimatePresence } from 'framer-motion'
-import clsx from 'clsx'
 import { Icon } from '@iconify/react'
 import '@/icons/registerRi'
 
